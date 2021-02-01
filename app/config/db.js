f1248bb8ecb4a45fdd8000f338226e5e80a9d4d0
Eric Sentongo <eric_sentongo@yahoo.com>
2021-02-01 18:55:32 +0300

    Update db.js

--- app/config/db.js
+++ app/config/db.js
@@ -8,7 +8,7 @@ var fs     = require('fs');
 //mysql db connection
 var connection = mysql.createPool({
     connectionLimit: 5, //important
-    host: '10.86.224.3',
+    host: '35.246.46.157',
     user: 'root',
     password: 'e60CeL5syAuPD5KM',
     database: 'mon4lx',
