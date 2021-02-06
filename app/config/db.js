ef43451c8162f98eb2cddf55574f1d17b9423a5f
Eric Sentongo <eric_sentongo@yahoo.com>
2021-02-06 10:25:42 +0300

    Update db.js

--- app/config/db.js
+++ app/config/db.js
@@ -3,22 +3,22 @@
  */
 
 var mysql = require('mysql');
-var fs     = require('fs');
+var fs = require('fs');
 
 //mysql db connection
 var connection = mysql.createPool({
     connectionLimit: 5, //important
-    // host: '35.246.46.157',
-    socketPath: '/cloudsql/rising-pen-293118:europe-west2:mon4lx-db',
+    host: '10.86.224.3',
+    // socketPath: '/cloudsql/rising-pen-293118:europe-west2:mon4lx-db',
     user: 'root',
     password: 'e60CeL5syAuPD5KM',
     database: 'mon4lx',
     debug: false,
-    ssl      : {
-        ca   : fs.readFileSync('./app/ssl/server-ca.pem'), // should be enough for AWS
-        key  : fs.readFileSync('./app/ssl/client-key.pem'), // required for google mysql cloud db
-        cert : fs.readFileSync('./app/ssl/client-cert.pem'), // required for google mysql cloud db
-  }
+    ssl: {
+        ca: fs.readFileSync('./app/ssl/server-ca.pem'), // should be enough for AWS
+        key: fs.readFileSync('./app/ssl/client-key.pem'), // required for google mysql cloud db
+        cert: fs.readFileSync('./app/ssl/client-cert.pem'), // required for google mysql cloud db
+    }
 });
 
 connection.getConnection(function (err) {
